bd5db431116bce630233e20b56bebe8c0fcfd6fe
Viktor <osipenko_vp@mail.ru>
2018-09-18 22:04:06 +0300

    выполнено easy

--- lesson-5/hw05_easy.py
+++ lesson-5/hw05_easy.py
@@ -3,8 +3,28 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+import sys
+import shutil
+
+for i in range(1, 10):
+    dir_path = os.path.join(os.getcwd(), 'dir_' + str(i))
+    try:
+        os.mkdir(dir_path)
+    except FileExistsError:
+        print('Такая директория существует')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+for i in os.listdir():
+    if os.path.isdir(i):
+        print(i)
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+name_new_file = os.path.split(sys.argv[0])
+name_new_file = '{}/new_{}'.format(name_new_file[0], name_new_file[1])
+shutil.copy(sys.argv[0], name_new_file)
+# print(name_new_file)

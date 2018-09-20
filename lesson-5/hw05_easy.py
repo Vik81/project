3494d08fa71502f84c1105f7dc93b180700099f6
Vik81 <osipenko_vp@mail.ru>
2018-09-20 15:24:18 +0300

    Add files via upload
    homework 5

--- lesson-5/hw05_easy.py
+++ lesson-5/hw05_easy.py
@@ -7,24 +7,31 @@ import os
 import sys
 import shutil
 
-for i in range(1, 10):
-    dir_path = os.path.join(os.getcwd(), 'dir_' + str(i))
+def createdir(n): # кол-во папок
     try:
-        os.mkdir(dir_path)
-    except FileExistsError:
-        print('Такая директория существует')
+        n = int(n)
+    except:
+        print('введите целое число')
+    for i in range(1, n + 1):
+        dir_path = os.path.join(os.getcwd(), 'dir_' + str(i))
+        try:
+            os.mkdir(dir_path)
+            print('Папка {} создана'.format(dir_path))
+        except FileExistsError:
+            print('Такая директория существует')
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
-for i in os.listdir():
-    if os.path.isdir(i):
+def lsdir():
+    for i in os.listdir():
+        # if os.path.isdir(i):   # по условию задачи должны бать только папки, но по normal - содержимое папки
         print(i)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-name_new_file = os.path.split(sys.argv[0])
-name_new_file = '{}/new_{}'.format(name_new_file[0], name_new_file[1])
-shutil.copy(sys.argv[0], name_new_file)
-# print(name_new_file)
+def cpfile():
+    name_new_file = os.path.split(sys.argv[0])
+    name_new_file = '{}/new_{}'.format(name_new_file[0], name_new_file[1])
+    shutil.copy(sys.argv[0], name_new_file)
--- lesson-5/hw05_normal.py
+++ lesson-5/hw05_normal.py
@@ -13,3 +13,45 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import hw05_easy
+
+def gotodir():
+    name_dir = input('введите путь, куда надо перейти: ')
+    if os.path.exists(name_dir):
+        os.chdir(name_dir)
+        print('перешел в папку: {}'.format(name_dir))
+    else:
+        print('Невозможно перейти в указанную папку. Папка не существует.')
+
+def deletedir():
+    name_dir = input('введите имя папки, которую надо удалить: ')
+    if os.path.exists(name_dir):
+        os.rmdir(name_dir)
+        print('папка {} удалена.'.format(name_dir))
+    else:
+        print('Невозможно удалить указанную папку. Папка не существует.')
+
+print('1. Перейти в папку')
+print('2. Просмотреть содержимое текущей папки')
+print('3. Удалить папку')
+print('4. Создать папку')
+try:
+    response = int(input('введите номер меню: '))
+except ValueError:
+    print('Введите число')
+    sys.exit()
+
+if response == 1:
+    gotodir()
+elif response == 2:
+    hw05_easy.lsdir()
+elif response == 3:
+    deletedir()
+elif response == 4:
+    hw05_easy.createdir(1)
+else:
+    print('неопознаная команда')
+

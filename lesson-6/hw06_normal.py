dfbfc1e3eca3e9e8e352f20ca3c3fe75200452d4
Viktor <osipenko_vp@mail.ru>
2018-09-23 22:29:46 +0300

    lesson-6

--- lesson-6/hw06_normal.py
+++ lesson-6/hw06_normal.py
@@ -10,3 +10,45 @@
 # Функция подсчета урона должна быть инкапсулирована
 # Вам надо описать игровой цикл так же через класс.
 # Создайте экземпляры классов, проведите бой. Кто будет атаковать первым оставляю на ваше усмотрение.
+
+class Person:
+    def __init__(self, name, armor=0.7, health=100, damage=50):
+        self._name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+
+    def calculate_damage(self, damage, armor):
+        return self.damage // self.armor
+
+    def attack(self, who_attack, who_defend):
+        damage = self.calculate_damage(who_attack.damage, who_defend.armor)
+        who_defend.health -= damage
+        # print('{} нанес {} урона {}, у того осталось {} жизней.'.format(who_attack.name, who_defend.name, damage,
+        #                                                                 who_defend.health))
+
+class Game:
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
+
+    def start_game(self, player, enemy):
+        last_attacker = self.player
+        while self.player.health > 0 and self.enemy.health > 0:
+            if last_attacker == self.player:
+                self.attack(enemy, player)
+                last_attacker = self.enemy
+            else:
+                self.attack(player, enemy)
+                last_attacker = self.player
+
+        if self.player.health > 0:
+            print('Игрок победил!')
+        else:
+            print('Враг победил!')
+
+class Player(Person):
+    pass
+
+class Enemy(Person):
+    pass
